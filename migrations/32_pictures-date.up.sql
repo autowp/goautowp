16827070261877825160da65cf3a7c9d6692d380
Dmitry P <dmitry@pereslegin.ru>
2019-06-18 00:13:00 +0300

    picture taken date

--- migrations/32_pictures-date.up.sql
+++ migrations/32_pictures-date.up.sql
@@ -1,4 +1,4 @@
 alter table pictures
-    add column taken_year year default null,
+    add column taken_year smallint default null,
     add column taken_month tinyint default null,
     add column taken_day tinyint default null;
