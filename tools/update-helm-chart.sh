e170bac5c0ec1c617b09f8f518bcae4e9d8a3bde
Dmitry P <dmitry@pereslegin.ru>
2022-07-01 00:12:57 +0300

    fix: replace image address with sed in sh

--- tools/update-helm-chart.sh
+++ tools/update-helm-chart.sh
@@ -2,21 +2,11 @@
 
 set -e
 
-templates=(
-  "goautowp-autoban.yaml"
-  "goautowp-listen-df-amqp.yaml"
-  "goautowp-listen-monitoring-amqp.yaml"
-  "goautowp-scheduler-daily.yaml"
-  "goautowp-scheduler-hourly.yaml"
-  "goautowp-scheduler-midnight.yaml"
-  "goautowp-serve-private.yaml"
-  "goautowp-serve-public.yaml"
+TEMPLATES="goautowp-autoban.yaml goautowp-listen-df-amqp.yaml goautowp-listen-monitoring-amqp.yaml goautowp-scheduler-daily.yaml goautowp-scheduler-hourly.yaml goautowp-scheduler-midnight.yaml goautowp-serve-private.yaml goautowp-serve-public.yaml"
 
-)
-
-for template in ${templates[@]}; do
-  sed -i -E "s/(image:[[:space:]]+registry\.pereslegin\.ru\/autowp\/goautowp:).+/image: $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG/" templates/$template
-  git add templates/$template
+for TEMPLATE in ${TEMPLATES}; do
+  sed -i -E "s/(image:[[:space:]]+registry\.pereslegin\.ru\/autowp\/goautowp:).+/image: $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG/" templates/$TEMPLATE
+  git add templates/$TEMPLATE
 done
 
 git commit -m "feat: Update go-backend to $CI_COMMIT_TAG"
