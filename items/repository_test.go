a99e38e7fc1fd4247f1caa142ab8e017d347e31a
Dmitry P <dmitry@pereslegin.ru>
2023-01-22 05:01:58 +0300

    test: Merge similar tests

--- items/repository_test.go
+++ items/repository_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTopBrandsListRu(t *testing.T) {
+func TestTopBrandsListRuZh(t *testing.T) {
 	t.Parallel()
 
 	cfg := config.LoadConfig("../")
@@ -24,57 +24,30 @@ func TestTopBrandsListRu(t *testing.T) {
 	ctx := context.Background()
 
 	repository := NewRepository(goquDB)
-	options := ListOptions{
-		Language: "ru",
-		Fields: ListFields{
-			Name:                true,
-			DescendantsCount:    true,
-			NewDescendantsCount: true,
-		},
-		TypeID:     []ItemType{BRAND},
-		Limit:      TopBrandsCount,
-		OrderBy:    []exp.OrderedExpression{goqu.I("descendants_count").Desc()},
-		SortByName: true,
+
+	langs := []string{"ru", "zh"}
+
+	for _, lang := range langs {
+		options := ListOptions{
+			Language: lang,
+			Fields: ListFields{
+				Name:                true,
+				DescendantsCount:    true,
+				NewDescendantsCount: true,
+			},
+			TypeID:     []ItemType{BRAND},
+			Limit:      TopBrandsCount,
+			OrderBy:    []exp.OrderedExpression{goqu.I("descendants_count").Desc()},
+			SortByName: true,
+		}
+		r, err := repository.List(ctx, options)
+		require.NoError(t, err)
+		require.NotEmpty(t, r)
+
+		c, err := repository.Count(ctx, options)
+		require.NoError(t, err)
+		require.Greater(t, c, 0)
 	}
-	r, err := repository.List(ctx, options)
-	require.NoError(t, err)
-	require.NotEmpty(t, r)
-
-	c, err := repository.Count(ctx, options)
-	require.NoError(t, err)
-	require.Greater(t, c, 0)
-}
-
-func TestTopBrandsListZh(t *testing.T) {
-	t.Parallel()
-
-	cfg := config.LoadConfig("../")
-	db, err := sql.Open("mysql", cfg.AutowpDSN)
-	require.NoError(t, err)
-
-	goquDB := goqu.New("mysql", db)
-	ctx := context.Background()
-
-	repository := NewRepository(goquDB)
-	options := ListOptions{
-		Language: "zh",
-		Fields: ListFields{
-			Name:                true,
-			DescendantsCount:    true,
-			NewDescendantsCount: true,
-		},
-		TypeID:     []ItemType{BRAND},
-		Limit:      TopBrandsCount,
-		OrderBy:    []exp.OrderedExpression{goqu.I("descendants_count").Desc()},
-		SortByName: true,
-	}
-	r, err := repository.List(ctx, options)
-	require.NoError(t, err)
-	require.NotEmpty(t, r)
-
-	c, err := repository.Count(ctx, options)
-	require.NoError(t, err)
-	require.Greater(t, c, 0)
 }
 
 func TestGetItemsNameAndCatnameShouldNotBeOmittedWhenDescendantsCountRequested(t *testing.T) {
