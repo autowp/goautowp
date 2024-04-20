6d90ed55f50467ec46721aa3e4acd4c042d0b1ee
Dmitry P <dmitry@pereslegin.ru>
2024-04-20 17:34:47 +0300

    test: cover listen & serve commands

--- app_test.go
+++ app_test.go
@@ -1,8 +1,10 @@
 package goautowp
 
 import (
+	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/autowp/goautowp/config"
 	"github.com/golang-migrate/migrate/v4"
@@ -29,4 +31,90 @@ func TestAutowpMigrations(t *testing.T) {
 	if !errors.Is(err, migrate.ErrNoChange) {
 		require.NoError(t, err)
 	}
+}
+
+func TestListenDuplicateFinderAMQP(t *testing.T) {
+	t.Parallel()
+
+	cfg := config.LoadConfig(".")
+
+	app := NewApplication(cfg)
+
+	ctx := context.Background()
+
+	done := make(chan bool)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- false
+	}()
+
+	err := app.ListenDuplicateFinderAMQP(ctx, done)
+	require.NoError(t, err)
+}
+
+func TestListenMonitoringAMQP(t *testing.T) {
+	t.Parallel()
+
+	cfg := config.LoadConfig(".")
+	app := NewApplication(cfg)
+	ctx := context.Background()
+
+	done := make(chan bool)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- false
+	}()
+
+	err := app.ListenMonitoringAMQP(ctx, done)
+	require.NoError(t, err)
+}
+
+func TestServeGRPC(t *testing.T) {
+	t.Parallel()
+
+	cfg := config.LoadConfig(".")
+	app := NewApplication(cfg)
+
+	done := make(chan bool)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- false
+	}()
+
+	err := app.ServeGRPC(done)
+	require.NoError(t, err)
+}
+
+func TestServePublic(t *testing.T) {
+	t.Parallel()
+
+	cfg := config.LoadConfig(".")
+	app := NewApplication(cfg)
+	ctx := context.Background()
+
+	done := make(chan bool)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- false
+	}()
+
+	err := app.ServePublic(ctx, done)
+	require.NoError(t, err)
+}
+
+func TestServePrivate(t *testing.T) {
+	t.Parallel()
+
+	cfg := config.LoadConfig(".")
+	app := NewApplication(cfg)
+	ctx := context.Background()
+
+	done := make(chan bool)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- false
+	}()
+
+	err := app.ServePrivate(ctx, done)
+	require.NoError(t, err)
 }
