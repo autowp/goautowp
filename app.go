1ae0b90df955a966424689da6d5eadb74f03e859
Dmitry P <dmitry@pereslegin.ru>
2024-07-11 15:49:43 +0300

    fix: temporary disable cleanups

--- app.go
+++ app.go
@@ -277,32 +277,32 @@ func (s *Application) SchedulerDaily(ctx context.Context) error {
 
 	logrus.Infof("Comments deleted: %d", affected)
 
-	affected, err = commentsRep.RefreshRepliesCount(ctx)
-	if err != nil {
-		logrus.Error(err.Error())
+	// affected, err = commentsRep.RefreshRepliesCount(ctx)
+	// if err != nil {
+	//	logrus.Error(err.Error())
+	//
+	//	return err
+	// }
+	//
+	// logrus.Infof("Replies refreshed: %d", affected)
 
-		return err
-	}
+	// affected, err = commentsRep.CleanBrokenMessages(ctx)
+	// if err != nil {
+	//	logrus.Error(err.Error())
+	//
+	//	return err
+	// }
+	//
+	// logrus.Infof("Clean broken: %d", affected)
 
-	logrus.Infof("Replies refreshed: %d", affected)
-
-	affected, err = commentsRep.CleanBrokenMessages(ctx)
-	if err != nil {
-		logrus.Error(err.Error())
-
-		return err
-	}
-
-	logrus.Infof("Clean broken: %d", affected)
-
-	affected, err = commentsRep.CleanTopics(ctx)
-	if err != nil {
-		logrus.Error(err.Error())
-
-		return err
-	}
-
-	logrus.Infof("Clean topics: %d", affected)
+	// affected, err = commentsRep.CleanTopics(ctx)
+	// if err != nil {
+	//	logrus.Error(err.Error())
+	//
+	//	return err
+	// }
+	//
+	// logrus.Infof("Clean topics: %d", affected)
 
 	return nil
 }
