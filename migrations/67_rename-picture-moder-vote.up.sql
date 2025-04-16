386dead7625260a3a0951a10445e646a3068810f
Dmitry P. <dmitry@pereslegin.ru>
2025-04-17 02:35:31 +0300

    fix: migration

--- migrations/67_rename-picture-moder-vote.up.sql
+++ migrations/67_rename-picture-moder-vote.up.sql
@@ -1,5 +1,6 @@
 ALTER TABLE pictures_moder_votes RENAME picture_moder_vote;
 ALTER TABLE articles RENAME article;
+ALTER TABLE pictures DROP COLUMN type;
 
 DROP TABLE IF EXISTS pictures_types;
 DROP TABLE IF EXISTS login_state;
